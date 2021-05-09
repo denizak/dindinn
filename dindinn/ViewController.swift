5a265b69d38563a5efa673ac2d4d33ecb6f59992
deni zakya <denizakya@gmail.com>
2021-05-09 20:46:15 +0700

    Initial Commit

---
+++ dindinn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dindinn
+//
+//  Created by Deni Zakya on 09/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
